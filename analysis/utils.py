44ba4c397b3b72006d2646c920006e81bf995ff8
EirikSoevik <eirik.ruben@gmail.com>
2022-05-14 13:59:56 +0200

    fixed fourier transformation and evaluation

--- analysis/utils.py
+++ analysis/utils.py
@@ -235,42 +235,86 @@ def fourier_analysis_all(midline_x, midline_y, T, plotting):
 
     return f_x1, f_y, N, f_y_total, phase
 
-def wave_number():
+def wave_number(phase, filtered_midlines_y, f_dom, sample_spacing, norm_x):
+    """k: wave number
 
-    #TODO: finish this
-    print()
+    k = frequency / seconds
+    :param norm_x:
+    :param phase:
+    """
 
-def fft_analysis(f_x, f_y_total, midlines_y, plotting):
+    time_dim, space_dim = filtered_midlines_y.shape
+    wave_length = np.zeros([space_dim, 1])
+    k = np.zeros([space_dim, 1])
+
+    # for s in range(space_dim):
+    #     #wave_length[s] = f_dom[s]/(time_dim*sample_spacing)
+    #     #k[s] = (2*np.pi) / wave_length[s]
+    #     k[s] = phase[s]/norm_x[s]
+    #     wave_length[s] = 2*np.pi / k[s]
+    k = (phase[-1] - phase[0])
+    wave_length = 2*np.pi/k
+
+    return wave_length, k
+
+def fft_analysis(f_x, f_y_total, midlines_y, phase, plotting):
     """ Analysis of fft signal, finds dominating freq., wave number, etc.
 
     Note that the dominant frequency might be due to the fish moving and so the lowest peak might need to be subtracted
     """
+
+    # Changeable variables
     wait = 1 #plotting animation
 
+    # Initialization
     time_it, space = f_y_total.shape
-    #midlines_y_filtered = np.zeros([time_it, space])
-
     m_time, m_space = midlines_y.shape
     f_dom = np.zeros([space])
     max_y_ind = np.zeros([space]).astype(int)
     filtered_midlines_y = np.zeros([m_time, m_space])
-    #filtered_midlines_y1 = {}
     old_y = np.zeros([m_time, m_space])
     max_y = np.zeros([space])
     T_dom = np.zeros([space])
+    phase_dom = np.zeros([space])
 
     nfy_time, nfy_space = f_y_total.shape
-    new_f_y = np.empty([nfy_time, nfy_space]).astype(complex)
-    #new_f_y = np.zeros([nfy_time, nfy_space], dtype='complex')
-    new_vec = np.empty([nfy_time//2]).astype(complex)
-    new_f_y = new_f_y*0
-    new_vec = new_vec*0
+    new_vec = np.zeros([nfy_time//2], dtype='complex')
 
     scale_factor = 1.5
     high_pass = 0#2
     low_pass = 6
 
+    ff_fil = np.zeros([m_time, m_space], dtype='complex')
+
     for s in range(space):
+
+        ######## claudio version
+        #s = 18
+        ff = f_y_total[:,s]
+        nfft = len(midlines_y[:,s])//2-1
+
+        a0 = ff[0]
+        anq = ff[nfft+1]
+        a1 = ff[1:nfft+1]
+        a2 = ff[2*nfft+2:nfft+1:-1]
+
+        ind_start = 3
+        ind_end = 10
+        b1 = np.zeros([len(a1)], dtype='complex')
+        b1[ind_start:ind_end] = a1[ind_start:ind_end]
+
+
+        ff_fil[0, s] = a0
+        ff_fil[1:len(b1)+1, s] = b1
+        ff_fil[len(b1)+1, s] = anq
+        ff_fil[len(b1)+2:-1, s] = np.conj(b1[-1:0:-1])
+
+
+        #save_dir = "my_data/mask_output_april_Apr-27-2022_1618/"
+        #from scipy.io import savemat
+        #savemat(save_dir + 'ff_fil2.mat', {'my_data2': ff_fil})
+        ###### claudio end
+
         new_vec = new_vec * 0
         f_y = np.abs(f_y_total[1:len(f_y_total)//2, s])
 
@@ -279,7 +323,7 @@ def fft_analysis(f_x, f_y_total, midlines_y, plotting):
         max_y_ind[s] = f_y.argmax()+1
         f_dom[s] = f_x[max_y_ind[s]]
         T_dom[s] = 1/f_dom[s]
-        phase_dom[s] = phase[]
+        phase_dom[s] = phase[max_y_ind[s],s]
 
         new_vec[high_pass:int(np.ceil(max_y_ind[s])*scale_factor)] = f_y_total[high_pass:int(np.ceil(max_y_ind[s])*scale_factor), s]
 
@@ -291,10 +335,13 @@ def fft_analysis(f_x, f_y_total, midlines_y, plotting):
         new_fy[nfy_time//2] = 0
         new_fy = np.append(new_fy,new_vec[-1:0:-1])
 
-        plt.plot(new_fy)
-        plt.show()
 
-        filtered_midlines_y[:, s] = ifft(new_fy)
+
+        #plt.plot(new_fy)
+        #plt.show()
+
+        filtered_midlines_y[:,s] = ifft(ff_fil[:,s])
+        #filtered_midlines_y[:, s] = ifft(new_fy)
         old_y[:,s] = ifft(f_y_total[:, s])
 
 
@@ -305,15 +352,11 @@ def fft_analysis(f_x, f_y_total, midlines_y, plotting):
         for freq in f_dom:
             print("Dominant frequency: " + str(freq))
 
-    import time
-    plotting=True
+
+    plotting=False#True
     if plotting:
         aplt.fft_evaluation(old_y,midlines_y,filtered_midlines_y,scale_factor, wait)
-    plotting=False
-
-
-
-    return f_dom, filtered_midlines_y, max_y
+    return f_dom, filtered_midlines_y, max_y, phase_dom
 
 
 def lateral_displacement(mean_length, midlines_y):
@@ -328,7 +371,7 @@ def lateral_displacement(mean_length, midlines_y):
 
     return y_displacement
 
-def curve_fit_second_order(x_vec, y_vec, body_length, order=2, output_length=100):
+def curve_fit(x_vec, y_vec, order=2, output_length=100):
     """Curve fitting of a second degree equation, here  the amplitudes of each point along the midline"""
 
 
@@ -340,3 +383,75 @@ def curve_fit_second_order(x_vec, y_vec, body_length, order=2, output_length=100
 
     return xfit, yfit, polynomial
 
+def combined_thingy(k, phase_dom, norm_x, plotting, my_title, save_dir, save=False):
+    """TODO: What to call this?"""
+
+    print("combinded thingy")
+    plt.figure()
+    plt.plot(norm_x,phase_dom)
+    plt.title("Phase of dominating frequency")
+    plt.xlabel("x/BL")
+    plt.ylabel("Phase")
+    plt.show()
+
+    Pm_vec = k*phase_dom*norm_x
+
+    #for i in range(6):
+    #    Pm_vec[i] = Pm_vec[i]
+
+    xfit, yfit, Pm = curve_fit(norm_x, Pm_vec, order=3, output_length=20)
+
+
+    if plotting:
+        aplt.thingy_plot(Pm_vec, norm_x, xfit, yfit, save_dir, my_title, save)
+
+    return xfit, yfit, Pm_vec, Pm
+
+def steady_motion(amp, f_dom, time_vec, Pm, x_vec):
+    """ finds steady motion array
+    # TODO: fix cos_part part
+    Note: treads f_dom as single number"""
+    x_vec = np.linspace(0,1,100)
+    time_dim = len(time_vec)
+    space_dim = len(x_vec)
+
+    Y = np.zeros([time_dim, space_dim])
+    cos_part = np.zeros([time_dim, space_dim])
+    part = np.zeros([time_dim, space_dim])
+
+    for s in range(len(x_vec)):
+        for t in range(len(time_vec)):
+            Y[t, s] = (amp[0]*x_vec[s]**2+amp[1]*x_vec[s]+amp[2])*np.cos(2*np.pi*f_dom*time_vec[t] + Pm[0]*x_vec[s]**3+Pm[1]*x_vec[s]**2+Pm[2]*x_vec[s]+Pm[3])
+            cos_part[t, s] = np.cos(2*np.pi*f_dom*time_vec[t] + Pm[0]*x_vec[s]**3+Pm[1]*x_vec[s]**2+Pm[2]*x_vec[s]+Pm[3])
+            part[t,s] = 2*np.pi*f_dom*time_vec[t] + Pm[0]*x_vec[s]**3+Pm[1]*x_vec[s]**2+Pm[2]*x_vec[s]+Pm[3]
+
+    #plt.figure()
+    #for t in range(time_dim):
+    #    plt.plot(Y[t,:])
+    #plt.show()
+
+    plt.figure()
+    for t in range(8):
+        plt.plot(cos_part[t*10,:])
+        #plt.plot(part[t,:])
+    plt.show()
+
+    ## Attempt to create polynomial from input polynomials
+    #Y = amp*(np.cos(2*np.pi*f_dom*time_vec + Pm))
+    #poly_amp = np.poly1d(amp)
+    #poly_Pm = np.poly1d(Pm)
+
+    #Y1 = poly_amp()*np.cos(2*np.pi*f_dom*time_vec+poly_Pm())
+
+    ## Attempt to fit polynomial to Y
+    #Y_poly = np.polyfit(Y)
+
+    return Y
+
+def phase_filter(phase_dom):
+    """ Phase-shifts the phases to make it physically correct"""
+
+    for i in range(6):
+        phase_dom[i] = phase_dom[i]+2*np.pi
+
+    return phase_dom
